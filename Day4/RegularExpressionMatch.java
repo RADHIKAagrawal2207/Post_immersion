d3034156450ccddeda9dec637f826fbfb35d3e98
RADHIKAagrawal2207 <radhikaagarwal227@gmail.com>
2025-08-01 13:52:34 +0530

    Add

---
+++ Day4/RegularExpressionMatch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return s.matches(p);
+    }
+}
+// Example 1:
+
+// Input: s = "aa", p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+// Example 2:
+
+// Input: s = "aa", p = "a*"
+// Output: true
+// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+// Example 3:
+
+// Input: s = "ab", p = ".*"
+// Output: true
+// Explanation: ".*" means "zero or more (*) of any character (.)".
