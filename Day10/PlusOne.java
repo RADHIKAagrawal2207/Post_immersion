e7f1a77a4fe2dbb006d2cf7ffee54367c1af529a
RADHIKAagrawal2207 <radhikaagarwal227@gmail.com>
2025-08-16 10:44:43 +0530

    fm

---
+++ Day10/PlusOne.java
@@ -0,0 +1,27 @@
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int arr[]=new int[n+1];
+        arr[0]=1;
+        return arr;
+    }
+}
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Incrementing by one gives 4321 + 1 = 4322.
+// Thus, the result should be [4,3,2,2].
+// Example 3:
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
