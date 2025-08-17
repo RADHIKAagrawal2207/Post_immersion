042feb1f9dafe1345ef34a06334932664f115576
RADHIKAagrawal2207 <radhikaagarwal227@gmail.com>
2025-08-17 23:45:27 +0530

    fm

---
+++ Day11/Parenthesisvalid.java
@@ -0,0 +1,29 @@
+package Post_immersion.Day11;
+
+import java.util.Stack;
+
+public class Parenthesisvalid {
+  public boolean isValid(String s) {
+    Stack<Character> stack=new Stack<>();
+    for (char c : s.toCharArray())
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+      else if (stack.isEmpty() || stack.pop() != c)
+        return false;
+    return stack.isEmpty();
+  }
+}
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
